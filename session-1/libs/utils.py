317bb2d3ceb8fa74103f362e09cea0579abddd67
Arn-O <rnd.bertrand@gmail.com>
2016-07-26 22:51:54 +0200

    refresh from cadl repo

--- session-1/libs/utils.py
+++ session-1/libs/utils.py
@@ -16,25 +16,20 @@ import os
 
 def imcrop_tosquare(img):
     """Make any image a square image.
-
     Parameters
     ----------
     img : np.ndarray
         Input image to crop, assumed at least 2d.
-
     Returns
     -------
     crop : np.ndarray
         Cropped image.
     """
-    if img.shape[0] > img.shape[1]:
-        extra = (img.shape[0] - img.shape[1]) // 2
-        crop = img[extra:-extra, :]
-    elif img.shape[1] > img.shape[0]:
-        extra = (img.shape[1] - img.shape[0]) // 2
-        crop = img[:, extra:-extra]
-    else:
-        crop = img
+    size = np.min(img.shape[:2])
+    extra = img.shape[:2] - size
+    crop = img
+    for i in np.flatnonzero(extra):
+        crop = np.take(crop, extra[i] // 2 + np.r_[:size], axis=i)
     return crop
 
 
