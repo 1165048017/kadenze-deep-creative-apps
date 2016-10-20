b82e7df2c99579053c7f50d81e78c1c01a70587e
Arn-O <rnd.bertrand@gmail.com>
2016-10-20 23:14:49 +0200

    add svg row and convert to png

--- final-project/libs/svg_utils.py
+++ final-project/libs/svg_utils.py
@@ -4,6 +4,8 @@ https://github.com/hardmaru/write-rnn-tensorflow
 """
 
 import svgwrite
+import matplotlib.pyplot as plt
+import subprocess
 
 def initiate(size_x, size_y, filename):
     """Initiate SVG drawing.
@@ -24,3 +26,33 @@ def initiate(size_x, size_y, filename):
     dwg = svgwrite.Drawing(filename, size=dims)
     dwg.add(dwg.rect(insert=(0, 0), size=dims,fill='white'))
     return dwg
+
+
+def add_row_strokes(dwg, abs_x, abs_y, data, svg_scale=10):
+
+    lift_pen = 1
+    p = "M%s,%s " % (abs_x, abs_y)
+    command = "m"
+
+    for i in range(len(data)):
+        if (lift_pen == 1):
+            command = "m"
+        elif (command != "l"):
+            command = "l"
+        else:
+            command = ""
+        x = float(data[i,0])/svg_scale
+        y = float(data[i,1])/svg_scale
+        lift_pen = data[i, 2]
+        p += command+str(x)+","+str(y)+" "
+
+    the_color = "black"
+    stroke_width = 1
+
+    dwg.add(dwg.path(p).stroke(the_color,stroke_width).fill("none"))
+
+
+def convert_to_img(filename_svg, filename_png, density):
+    convert = subprocess.run(['convert', '-density', str(density), filename_svg, filename_png])
+    img = plt.imread(filename_png)
+    return img
